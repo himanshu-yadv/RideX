b098302b5ab27532bb58e27a7e0a303800099bf1
Himanshu Yadav <himanshu_yadav.1306@outlook.com>
2025-07-10 23:22:32 +0530

    Initial Commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -76,10 +76,11 @@ const Footer = () => {
                 <div>
                     <h2 className='text-base font-medium text-gray-800 uppercase'>Contact</h2>
                     <ul className='mt-3 flex flex-col gap-1.5'>
-                        <li>1234 Luxury Drive</li>
-                        <li>San Francisco, CA 94107</li>
-                        <li>+1 234 567890</li>
-                        <li>info@example.com</li>
+                        <li>221B MG Road</li>
+                        <li>Delhi, India 110001</li>
+                        <li>+91 8840488260</li>
+                        <li>info@carrental.in</li>
+
                     </ul>
                 </div>
 
